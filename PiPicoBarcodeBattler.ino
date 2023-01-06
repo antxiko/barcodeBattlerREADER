809cc0ae3e680a33f6236d2f0b299bb7bcea581d
Antxiko Gorjon <antxiko@gmail.com>
2023-01-06 12:43:07 +0100

    Support for Raspberry Pi Pico
    Light code retouches, Pi Pico has a perfect microsecond timing so there is no hard coded delays, the Barcode sends the Churro bits by 680 microseconds / bit speed, the Atmega328 has a 3mS +- margin, this is 0 in the Pico. The code is working in Arduino IDE with Pico board extension, tested on Arduino IDE 2.0.2.

---
+++ PiPicoBarcodeBattler.ino
@@ -0,0 +1,62 @@
+
+#define barcodePIN 9      //the pin thats read de Barcode Exp. Port.
+
+int recibido;             //receibed data variable, if 1 goes to read the churro
+int churro[205];          //the lengt of the barcode bit stream + 5 more bits
+
+void setup() {
+    pinMode(barcodePIN, INPUT_PULLUP);   //Barcode PIN as INPUT whith internal pull up resistor
+    Serial.begin(9600);                  //Start the serial
+}
+
+void loop() {
+    leerEntrada();                       //Whe read the arduino ext port.
+}
+
+void leerEntrada(){
+  //churro[0] = 0;                            //first bit of the churro is always 0
+    int anterior = 1;                       //if last bit is 1 we can start reading
+    int recibido = digitalRead(barcodePIN); //read again de Barcode Exp. Port. 
+    if (anterior != recibido) {             //If value changues, go and try reading the churro
+      //delayMicroseconds(840);               //We wait 840 micros, waiting for the next bit
+      leerChurro();                         //Calls churro reading function
+      imprimirChurro();                     //Print the bit stream info, BINARI & ASCII
+    }
+  
+}
+
+void leerChurro(){                          //This function reads all the 205 bits of the churro bit stream
+
+    for (int i = 0; i < 205; i++)
+      {
+      recibido = digitalRead(barcodePIN);
+      churro[i] = recibido;
+      delayMicroseconds(840);     
+    }
+}
+
+void imprimirChurro(){                      //This function prints the bit stream in binary and then in ASCII
+    Serial.println("Esto es lo que hemos leido desde el Barcode con una Pi Pico");
+  for (int c=0; c<205;c++)
+    {
+    Serial.print(churro[c]);
+    }
+    Serial.println();
+    Serial.println("0000011001000101100100000110010100011001001001100100000110010111011001011001100101100110010011011001010101100100000110010100011001010100010100000101010111100101011000010100001001010101100001001010000111111");
+    Serial.println();
+    Serial.println("Esto es la info de la carta en ASCII");
+    for (int a = 1; a < 191; a=a+10)
+      {
+        imprimirAscii(a);
+      }
+    Serial.println();
+}
+
+void imprimirAscii(int bitChurro){        //This function puts the binary value of the bit stream bits to byte "caracter"
+  byte caracter = 0;                      //and then prints it as ASCII Character
+  for (int o = 0; o < 8; o++){
+    bitWrite(caracter,o,churro[bitChurro+o]);
+  }
+  Serial.print(char(caracter));
+  //Serial.print(caracter);
+}
